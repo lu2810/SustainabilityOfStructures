5c4e4613854b4a8587679f689711d3aa30547ad5
lu2810 <luisa.albertini@ibk.baug.ethz.ch>
2024-12-04 09:46:35 +0100

    Update struct_analysis.py
    Inline comments

--- struct_analysis.py
+++ struct_analysis.py
@@ -162,9 +162,9 @@ class RectangularWood(SupStrucRectangular):
         super().__init__(b, h, phi)
         self.wood_type = wood_type
         mu_el, vu_el = self.calc_strength_elast(wood_type.fmd, wood_type.fvd)
-        self.mu_max, self.mu_min = [mu_el, mu_el]
+        self.mu_max, self.mu_min = [mu_el, mu_el]   #Readme: Why is this needed for wood?
         self.vu = vu_el
-        self.qs_class_n, self.qs_class_p = [3, 3]
+        self.qs_class_n, self.qs_class_p = [3, 3]   #Readme: What is this needed for?
         self.g0k = self.calc_weight(wood_type.weight)
         self.ei1 = self.wood_type.Emmean*self.iy  # elastic stiffness concrete (uncracked behaviour) [Nm^2]
         self.co2 = self.a_brutt * self.wood_type.GWP * self.wood_type.density  # [kg_CO2_eq/m]
